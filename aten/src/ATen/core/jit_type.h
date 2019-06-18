WeiHao97/pytorch_sparse

[JIT] Introduce SerializableType
gh-metadata: pytorch pytorch 21835 gh/jamesr66a/3/head

aten/src/ATen/core/jit_type.h
@@ -1376,6 +1376,7 @@ matchTypeVariables(TypePtr formal, TypePtr actual, TypeEnv& type_env);
 
 CAFFE2_API TypePtr evalTypeVariables(TypePtr type, TypeEnv & type_env);
 
+
 /**
  * User Defined Types
  */
@@ -1384,31 +1385,15 @@ struct ClassType;
 using ClassTypePtr = std::shared_ptr<ClassType>;
 using ::torch::jit::script::CompilationUnit;
 using ::torch::jit::Function;
+struct SerializableType;
+using SerializableTypePtr = std::shared_ptr<SerializableType>;
 
-// This represents a class in TorchScript.
-struct CAFFE2_API ClassType : public Type {
-  // Create a class type with name `name` and its methods stored in `cu`.
-  static ClassTypePtr create(
-      QualifiedName qualifiedName,
-      std::shared_ptr<CompilationUnit> cu);
+struct CAFFE2_API SerializableType : public Type {
+  SerializableType(TypeKind tk, c10::QualifiedName qualifiedName)
+    : Type(tk)
+    , name_(qualifiedName) {}
 
-  // Create a type representing a Module,
-  // These do not have methods, and are not globally registered
-  static ClassTypePtr createModuleType(std::shared_ptr<CompilationUnit> module);
-
-  DEFINE_IS_SUBCLASS(ClassType);
-  bool operator==(const Type& rhs) const override {
-    if (auto user_rhs = rhs.cast<ClassType>()) {
-      return name_ == user_rhs->name_;
-    }
-    return false;
-  }
-
-  std::string str() const override {
-    return std::string("ClassType<") + name_.name() + ">";
-  }
-
-  std::string python_str() const override {
+  std::string python_str() const {
     return name_.qualifiedName();
   }
 
@@ -1422,6 +1407,45 @@ struct CAFFE2_API ClassType : public Type {
 
   std::string basename() const {
     return name_.name();
+  }
+
+  virtual std::vector<Function*> methods() const = 0;
+
+ private:
+  // Fully qualified name of type (note that this has to be globally unique).
+  // Looks like: "foo.bar.Baz".
+  QualifiedName name_;
+
+  friend class ClassType;
+};
+
+
+
+// This represents a class in TorchScript.
+struct CAFFE2_API ClassType : public SerializableType {
+  // Create a class type with name `name` and its methods stored in `cu`.
+  static ClassTypePtr create(
+      QualifiedName qualifiedName,
+      std::shared_ptr<CompilationUnit> cu);
+
+  // Create a type representing a Module,
+  // These do not have methods, and are not globally registered
+  static ClassTypePtr createModuleType(std::shared_ptr<CompilationUnit> module);
+
+  DEFINE_IS_SUBCLASS(ClassType);
+  bool operator==(const Type& rhs) const override {
+    if (auto user_rhs = rhs.cast<ClassType>()) {
+      return qualname() == user_rhs->qualname();
+    }
+    return false;
+  }
+
+  std::string str() const override {
+    return std::string("ClassType<") + basename() + ">";
+  }
+
+  std::string python_str() const override {
+    return qualname();
   }
 
   TypePtr getAttribute(const std::string& name) const {
@@ -1455,7 +1479,7 @@ struct CAFFE2_API ClassType : public Type {
   std::shared_ptr<Function> getMethod(const std::string& name) const;
   CompilationUnit& compilation_unit();
   const CompilationUnit& compilation_unit() const;
-  std::vector<Function*> methods() const;
+  std::vector<Function*> methods() const override;
 
 
   size_t numAttributes() const {
@@ -1510,10 +1534,6 @@ struct CAFFE2_API ClassType : public Type {
 
  private:
   ClassType(QualifiedName name, std::shared_ptr<CompilationUnit> cu);
-
-  // Fully qualified name of type (note that this has to be globally unique).
-  // Looks like: "foo.bar.Baz".
-  QualifiedName name_;
 
   // Mapping of attribute names -> their type.
   // NOTE: this does not contain methods, which are stored in the module

torch/csrc/jit/passes/python_print.h
@@ -17,7 +17,7 @@ TORCH_API void PythonPrint(
     const Function& callee,
     bool is_method,
     std::vector<at::Tensor>& tensor_table,
-    std::vector<ClassTypePtr>& class_table,
+    std::vector<c10::SerializableTypePtr>& class_table,
     bool enforce_importable = false);
 
 TORCH_API void PythonPrint(
@@ -25,14 +25,14 @@ TORCH_API void PythonPrint(
     const script::CompilationUnit& cu,
     bool is_method,
     std::vector<at::Tensor>& tensor_table,
-    std::vector<ClassTypePtr>& class_table,
+    std::vector<c10::SerializableTypePtr>& class_table,
     bool enforce_importable);
 
 TORCH_API void PythonPrint(
     std::ostream& out,
-    const ClassTypePtr& classType,
+    const c10::SerializableTypePtr& classType,
     std::vector<at::Tensor>& tensor_table,
-    std::vector<ClassTypePtr>& class_table,
+    std::vector<c10::SerializableTypePtr>& class_table,
     bool enforce_importable = false);
 
 TORCH_API bool printerHasSpecialCaseFor(c10::Symbol sym);

torch/csrc/jit/script/init.cpp
@@ -532,7 +532,7 @@ void initJitScriptBindings(PyObject* module) {
           [](Module& self) {
             std::ostringstream ss;
             std::vector<at::Tensor> tensors;
-            std::vector<ClassTypePtr> classes;
+            std::vector<c10::SerializableTypePtr> classes;
             PythonPrint(
                 ss,
                 self.class_compilation_unit(),
@@ -609,7 +609,7 @@ void initJitScriptBindings(PyObject* module) {
           [](Function& self) {
             std::ostringstream ss;
             std::vector<at::Tensor> tensors;
-            std::vector<ClassTypePtr> classes;
+            std::vector<c10::SerializableTypePtr> classes;
             PythonPrint(ss, self, false, tensors, classes, false);
             return ss.str();
           })
@@ -643,7 +643,7 @@ void initJitScriptBindings(PyObject* module) {
       .def_property_readonly("code", [](Method& self) {
         std::ostringstream ss;
         std::vector<at::Tensor> tensors;
-        std::vector<ClassTypePtr> classes;
+        std::vector<c10::SerializableTypePtr> classes;
         PythonPrint(ss, self.function(), true, tensors, classes, false);
         return ss.str();
       });
@@ -768,7 +768,7 @@ void initJitScriptBindings(PyObject* module) {
   m.def("_jit_python_print", [](py::object obj) {
     std::ostringstream ss;
     std::vector<at::Tensor> constants;
-    std::vector<ClassTypePtr> classes;
+    std::vector<c10::SerializableTypePtr> classes;
     if (auto self = as_module(obj)) {
       PythonPrint(
           ss, self->class_compilation_unit(), true, constants, classes, true);

itomatik/pytorch

aten/src/ATen/core/jit_type.h
@@ -844,6 +844,37 @@ private:
   FutureType(TypePtr elem) : SingleElementType(elem) {}
 };
 
+using ::torch::jit::Function;
+struct NamedType;
+using NamedTypePtr = std::shared_ptr<NamedType>;
+
+struct CAFFE2_API NamedType : public Type {
+  NamedType(TypeKind tk, c10::QualifiedName qualifiedName)
+    : Type(tk)
+    , name_(qualifiedName) {}
+
+  std::string python_str() const {
+    return name_.qualifiedName();
+  }
+
+  std::string qualname() const {
+    return name_.qualifiedName();
+  }
+
+  std::string qualifier() const {
+    return name_.prefix();
+  }
+
+  std::string basename() const {
+    return name_.name();
+  }
+
+ protected:
+  // Fully qualified name of type (note that this has to be globally unique).
+  // Looks like: "foo.bar.Baz".
+  QualifiedName name_;
+};
+
 struct TupleType;
 using TupleTypePtr = std::shared_ptr<TupleType>;
 using OptNameList = c10::optional<std::vector<std::string>>;
@@ -1376,6 +1407,7 @@ matchTypeVariables(TypePtr formal, TypePtr actual, TypeEnv& type_env);
 
 CAFFE2_API TypePtr evalTypeVariables(TypePtr type, TypeEnv & type_env);
 
+
 /**
  * User Defined Types
  */
@@ -1383,10 +1415,9 @@ CAFFE2_API TypePtr evalTypeVariables(TypePtr type, TypeEnv & type_env);
 struct ClassType;
 using ClassTypePtr = std::shared_ptr<ClassType>;
 using ::torch::jit::script::CompilationUnit;
-using ::torch::jit::Function;
 
 // This represents a class in TorchScript.
-struct CAFFE2_API ClassType : public Type {
+struct CAFFE2_API ClassType : public NamedType {
   // Create a class type with name `name` and its methods stored in `cu`.
   static ClassTypePtr create(
       QualifiedName qualifiedName,
@@ -1395,29 +1426,17 @@ struct CAFFE2_API ClassType : public Type {
   DEFINE_IS_SUBCLASS(ClassType);
   bool operator==(const Type& rhs) const override {
     if (auto user_rhs = rhs.cast<ClassType>()) {
-      return name_ == user_rhs->name_;
+      return qualname() == user_rhs->qualname();
     }
     return false;
   }
 
   std::string str() const override {
-    return std::string("ClassType<") + name_.name() + ">";
+    return std::string("ClassType<") + basename() + ">";
   }
 
   std::string python_str() const override {
-    return name_.qualifiedName();
-  }
-
-  std::string qualname() const {
-    return name_.qualifiedName();
-  }
-
-  std::string qualifier() const {
-    return name_.prefix();
-  }
-
-  std::string basename() const {
-    return name_.name();
+    return qualname();
   }
 
   TypePtr getAttribute(const std::string& name) const {
@@ -1510,10 +1529,6 @@ struct CAFFE2_API ClassType : public Type {
 
  private:
   ClassType(QualifiedName name, std::shared_ptr<CompilationUnit> cu, bool is_module);
-
-  // Fully qualified name of type (note that this has to be globally unique).
-  // Looks like: "foo.bar.Baz".
-  QualifiedName name_;
 
   // Mapping of attribute names -> their type.
   // NOTE: this does not contain methods, which are stored in the module

torch/csrc/jit/passes/python_print.h
@@ -17,7 +17,7 @@ TORCH_API void PythonPrint(
     const Function& callee,
     bool is_method,
     std::vector<at::Tensor>& tensor_table,
-    std::vector<ClassTypePtr>& class_table,
+    std::vector<c10::NamedTypePtr>& class_table,
     bool enforce_importable = false);
 
 TORCH_API void PythonPrint(
@@ -25,14 +25,14 @@ TORCH_API void PythonPrint(
     const script::CompilationUnit& cu,
     bool is_method,
     std::vector<at::Tensor>& tensor_table,
-    std::vector<ClassTypePtr>& class_table,
+    std::vector<c10::NamedTypePtr>& class_table,
     bool enforce_importable);
 
 TORCH_API void PythonPrint(
     std::ostream& out,
-    const ClassTypePtr& classType,
+    const c10::NamedTypePtr& classType,
     std::vector<at::Tensor>& tensor_table,
-    std::vector<ClassTypePtr>& class_table,
+    std::vector<c10::NamedTypePtr>& class_table,
     bool enforce_importable = false);
 
 TORCH_API bool printerHasSpecialCaseFor(c10::Symbol sym);

torch/csrc/jit/script/init.cpp
@@ -532,7 +532,7 @@ void initJitScriptBindings(PyObject* module) {
           [](Module& self) {
             std::ostringstream ss;
             std::vector<at::Tensor> tensors;
-            std::vector<ClassTypePtr> classes;
+            std::vector<c10::NamedTypePtr> classes;
             PythonPrint(
                 ss,
                 *self.class_compilation_unit(),
@@ -609,7 +609,7 @@ void initJitScriptBindings(PyObject* module) {
           [](Function& self) {
             std::ostringstream ss;
             std::vector<at::Tensor> tensors;
-            std::vector<ClassTypePtr> classes;
+            std::vector<c10::NamedTypePtr> classes;
             PythonPrint(ss, self, false, tensors, classes, false);
             return ss.str();
           })
@@ -635,7 +635,7 @@ void initJitScriptBindings(PyObject* module) {
       .def_property_readonly("code", [](Method& self) {
         std::ostringstream ss;
         std::vector<at::Tensor> tensors;
-        std::vector<ClassTypePtr> classes;
+        std::vector<c10::NamedTypePtr> classes;
         PythonPrint(ss, self.function(), true, tensors, classes, false);
         return ss.str();
       });
@@ -760,7 +760,7 @@ void initJitScriptBindings(PyObject* module) {
   m.def("_jit_python_print", [](py::object obj) {
     std::ostringstream ss;
     std::vector<at::Tensor> constants;
-    std::vector<ClassTypePtr> classes;
+    std::vector<c10::NamedTypePtr> classes;
     if (auto self = as_module(obj)) {
       PythonPrint(
           ss, *self->class_compilation_unit(), true, constants, classes, true);